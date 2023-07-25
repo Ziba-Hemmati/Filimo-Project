a140d3e529aaf389955ad7c0359474c27f51e672
narges <nargesmanafii@gmail.com>
2023-07-26 01:35:51 +0330

    ref:refactor mobile first part of how watch

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
-import { getLocalData } from "../lib/localdata";
+import HowWatch from "@/components/mainPage/howWatch/HowWatch";
 import { MainBanner } from "@/components/mainPage/main-banner";
+import { getLocalData } from "../lib/localdata";
 const data = await getLocalData();
 const movie = data.movies;
 
 const Home = () => {
   return <div>
     <MainBanner movies={movie} />
+    <HowWatch/>
   </div>;
 };
 
---
+++ components/mainPage/howWatch/HowWatch.jsx
@@ -0,0 +1,53 @@
+
+const HowWatch = () =>{
+    return(
+        <div className="py-16">
+            <div className="px-3">
+                <div className="text-white text-base font-iran-sans mb-6">
+                    <h2>
+                        چطور با تلویزیون، فیلیمو تماشا کنم؟
+                    </h2>
+                </div>
+                <div className="text-gray-200 mb-3 pb-4 text-[.7rem] leading-6">
+                    <p>
+                        ما برای راحتی شما و تماشای فیلیمو روی تلویزیون‌های مختلف 6 روش را فراهم کردیم.
+                        هرکدام از این 6 روش یک ویدیو آموزشی دارند که می‌توانید از لینک زیر، 
+                        آن‌ها را تماشا کنید و روشی که با تلویزیون شما سازگار است را انتخاب کنید.
+                    </p>
+                </div>
+                <div className="w-100">
+                    <img src="	https://www.filimo.com/assets/web/ui/img-ynsexKiWcOkSOPureyAnA/campaigns/nosub-v_new/TVBG-yaghi.png" alt="" />
+                </div>
+                <div className="">
+                    <div className="flex mt-4">
+                        <div className="w-100 ml-3">
+                            <img src="https://www.filimo.com/assets/web/ui/img-ynsexKiWcOkSOPureyAnA/campaigns/nosub-v_new/TV.png"  width={36}/>
+                        </div>
+                        <div className="">
+                            <div className=" text-gray-200 font-iran-sans-medium font-medium mb-4">
+                                <p>تلویزیون</p>
+                            </div>
+                            <div className="text-gray-300 mb-8 text-[.7rem]">
+                                <p>Amazon Fire TV - LG TVs - Chrome Cast - Apple TV - Android TV devices - Samsung</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex">
+                        <div className="w-100 ml-3 ">
+                            <img src="https://www.filimo.com/assets/web/ui/img-ynsexKiWcOkSOPureyAnA/campaigns/nosub-v_new/androidTV.png"  width={36}/>
+                        </div>
+                        <div>
+                            <div className="text-gray-200 font-iran-sans-medium font-medium mb-4">
+                                <p>اندروید تی‌وی</p>
+                            </div>
+                            <div className="text-gray-300 mb-8 text-[.7rem]">
+                                <p>NVIDIA - amazon - xiaomi - minix - skystream - turewell - ematic - humax - matricom</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default HowWatch;
