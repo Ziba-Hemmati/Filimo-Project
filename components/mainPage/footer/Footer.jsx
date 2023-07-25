06272ee6e7f637634e944aa73e8f29da4a6076a6
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-25 20:10:23 +0330

    ref: refactor footer and add items component

--- components/mainPage/footer/Footer.jsx
+++ components/mainPage/footer/Footer.jsx
@@ -1,37 +1,18 @@
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false;
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import Items from "../items/Items";
+const text1 = "هزاران فیلم و سریال خارجی (دوبله و زیرنویس)";
+const text2 = "هزاران انیمیشن و کارتون برای کودکان";
+const text3 = "پشتیبانی 24 ساعته برای راهنمایی شما";
 
 const Footer = () => {
   return (
     <div className=" bg-green-900 w-full flex items-center justify-center text-center ">
       <div className="flex flex-col items-center">
-        <img src="https://www.filimo.com/assets/web/ui/img-ynsexKiWcOkSOPureyAnA/campaigns/nosub-v_new/logo_text.png" alt="logo-filimo" className="mb-14 mt-8 md:mt-16" />
-        <ul className="text-gray-300 text-xs flex flex-col items-center lg:flex-row lg:text-sm lg:pb-16">
-          <div className="flex items-center ">
-            <FontAwesomeIcon
-              icon={faCheck}
-              className="text-green-500 p-1 bg-green-800 rounded-full ml-2"
-            />
-            <li>هزاران فیلم و سریال خارجی (دوبله و زیرنویس)</li>
-          </div>
-          <div className="flex items-center leading-[3rem] lg:mx-[2rem]">
-            <FontAwesomeIcon
-              icon={faCheck}
-              className="text-green-500 p-1 bg-green-800 rounded-full ml-2 "
-            />
-            <li>هزاران انیمیشن و کارتون برای کودکان</li>
-          </div>
-          <div className="flex items-center pb-16 lg:pb-0">
-            <FontAwesomeIcon
-              icon={faCheck}
-              className="text-green-500 p-1 bg-green-800 rounded-full ml-2"
-            />
-            <li>پشتیبانی 24 ساعته برای راهنمایی شما</li>
-          </div>
-        </ul>
+        <img
+          src="https://www.filimo.com/assets/web/ui/img-ynsexKiWcOkSOPureyAnA/campaigns/nosub-v_new/logo_text.png"
+          alt="logo-filimo"
+          className="mb-14 mt-8 md:mt-16"
+        />
+        <Items text1={text1} text2={text2} text3={text3} />
       </div>
     </div>
   );
