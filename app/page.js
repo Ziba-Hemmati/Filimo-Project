148a655094b0dabde6db58c33c02180b321e2040
javad7721 <m.javad7721@gmail.com>
2023-07-26 00:40:51 +0330

    feat:main Frequently Asked Questions

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import { getLocalData } from "../lib/localdata";
+import Question from "@/components/mainPage/question/Question";
 
 const data = await getLocalData();
 const movie = data.movies;
 
 const Home = () => {
-  return <div></div>;
+  return <div>
+    <Question/>
+  </div>;
 };
 
 export default Home;
