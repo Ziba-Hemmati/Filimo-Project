6181683215280427c35b27634eb42d81eabcecff
narges <nargesmanafii@gmail.com>
2023-07-25 21:08:10 +0330

    ref: refactor part of children world

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
+import ChildrenWorld from "@/components/mainPage/children/ChildrenWorld";
 import { getLocalData } from "../lib/localdata";
 
 const data = await getLocalData();
 const movie = data.movies;
 
 const Home = () => {
-  return <div></div>;
+  return <div>
+    <ChildrenWorld/>
+  </div>;
 };
 
 export default Home;
---
+++ components/mainPage/children/ChildrenWorld.jsx
@@ -0,0 +1,17 @@
+
+const ChildrenWorld = () =>{
+    return(
+        <div className="pt-10 px-2.5 bg-gradient-to-r from-black-950 to-blue-800">
+            <div className="text-white text-iran-sans-medium pb-6 text-xl">
+                <p>دنیایی متنوع از فیلم و کارتون‌های کودکانه</p>
+            </div>
+            <div className="text-gray-300 text-sm pb-8">
+                <p>ساخت فیلیمو کودک این امکان را می‌دهد که با انتخاب سن فرزندتان، یک فضای امن برای تماشای کودک بسازید؛ یعنی محتوای مخصوص سن خودش را ببیند و به بقیه فیلم و سریال‌ها دسترسی نداشته‌باشد.</p>
+            </div>
+            <div className="flex items-center justify-center w-[100%]">
+                <img src="	https://www.filimo.com/assets/web/ui/img-ynsexKiWcOkSOPureyAnA/campaigns/nosub-v_new/bg-kids.png" alt=""  />
+            </div>
+        </div>
+    )
+}
+export default ChildrenWorld;
