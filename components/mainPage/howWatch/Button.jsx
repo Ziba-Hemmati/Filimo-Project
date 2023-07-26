64f1aa44c7075aaf75e041dcf4b0b3700bd1458e
narges <nargesmanafii@gmail.com>
2023-07-26 03:37:41 +0330

    ref:refactor some tailwindcss

--- components/mainPage/howWatch/Button.jsx
+++ components/mainPage/howWatch/Button.jsx
@@ -6,7 +6,7 @@ import {faChevronLeft} from "@fortawesome/free-solid-svg-icons"
 const Button = () =>{
     return(
         <div >
-            <button className="bg-gray-500 text-white px-7 ml-6 py-2.5 rounded-lg text-[.7rem]">
+            <button className="w-[100%] bg-gray-500 text-white py-3 rounded-lg text-[.7rem] transition-all duration-400 ease-in-out hover:bg-gray-400 lg:w-[30%] xl:w-[25%]">
             ویدیوهای آموزشی تماشا با تلویزیون 
             <FontAwesomeIcon className="text-white text-sm mr-2 text-center" icon={faChevronLeft}/>
             </button>
