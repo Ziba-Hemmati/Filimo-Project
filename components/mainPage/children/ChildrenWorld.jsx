85d8ac6c70ff4805dc06c5e504993140839fee5f
narges <nargesmanafii@gmail.com>
2023-07-26 12:28:18 +0330

    ref:refactor tailwind part of children world

--- components/mainPage/children/ChildrenWorld.jsx
+++ components/mainPage/children/ChildrenWorld.jsx
@@ -1,15 +1,21 @@
 
 const ChildrenWorld = () =>{
     return(
-        <div className="pt-10 px-2.5 bg-gradient-to-r from-black-950 to-blue-800">
+        <div className="px-2.5 py-16 bg-gradient-to-r from-black-950 to-blue-800  md:flex flex-row-reverse xl:px-24"> 
+            <div className="md:max-w-[47%] md:m-14">
             <div className="text-white text-iran-sans-medium pb-6 text-xl">
                 <p>دنیایی متنوع از فیلم و کارتون‌های کودکانه</p>
             </div>
             <div className="text-gray-300 text-sm pb-8">
                 <p>ساخت فیلیمو کودک این امکان را می‌دهد که با انتخاب سن فرزندتان، یک فضای امن برای تماشای کودک بسازید؛ یعنی محتوای مخصوص سن خودش را ببیند و به بقیه فیلم و سریال‌ها دسترسی نداشته‌باشد.</p>
             </div>
-            <div className="flex items-center justify-center w-[100%]">
-                <img src="	https://www.filimo.com/assets/web/ui/img-ynsexKiWcOkSOPureyAnA/campaigns/nosub-v_new/bg-kids.png" alt=""  />
+            <div className="bg-white text-center inline-blpck rounded-lg mb-6 text-xs">
+            <button className="py-2 px-2 w-[50%] ">فیلیمو کودک بساز</button>
+            <button className="py-2 px-2 w-[50%] ">تماشای فیلیمو کودک</button>
+            </div>
+            </div>
+            <div className="flex items-center justify-center w-[90%] md:mr-12 md:max-[47%] xl:mr-[9rem] ">
+                <img src="https://www.filimo.com/assets/web/ui/img-ynsexKiWcOkSOPureyAnA/campaigns/nosub-v_new/bg-kids.png"  />
             </div>
         </div>
     )
