4cb5de7d5be54f63c4872538dfb189c3394f4228
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-27 18:27:07 +0330

    feat: add hover to posters

--- components/movies/Movies.jsx
+++ components/movies/Movies.jsx
@@ -1,46 +1,34 @@
-"use client";
-import { useState } from "react";
-
 const Movies = ({ movie }) => {
-  // const [isShow, setIsShow] = useState(false);
-
   return (
     <div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 text-white p-[10px] ">
-        {movie.map(({ poster, id, faName, genre, year, country, isDub }) => {
-          return (
-            <div key={id} className=" min-h-[200px]">
-              <div className="relative">
-                <img
-                  className="w-full h rounded-[4px] p-[5px]"
-                  src={poster}
-                  // onMouseOver={() => setIsShow((prev) => !prev)}
-                />
-                <p className="text-[.7rem]">{faName}</p>
-                <div className="bg-black-950 opacity-0 absolute top-0 right-0 w-full h-full ">
-                  <p>{genre}</p>
-                  <p>
-                    {year} - {country} {isDub && " - دوبله شده"}
-                  </p>
-                </div>
-
-                {/* {isShow && (
-                  <div
-                    key={id}
-                    className="bg-black-600 bg-opacity-[.6] absolute w-[100%] h-[100%] z-10 top-[5px] right-[5px] text-[.7rem] "
-                  >
-                    <div className="absolute bottom-[30px]">
-                      <p>{genre}</p>
-                      <p>
-                        {year} - {country} {isDub && " - دوبله شده"}
-                      </p>
+      <h3 className="text-white my-6 px-[10px] lg:px-[30px]">فیلم</h3>
+      <div className="grid grid-cols-2 gap-2 gap-y-12 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 text-white p-[10px] text-[.7rem] lg:px-[30px]">
+        {movie.map(
+          ({ poster, id, faName, genre, year, country, isDub, rate }) => {
+            return (
+              <div key={id} className="relative">
+                <img className="w-full h-full rounded-[4px]" src={poster} />
+                <div className=" bg-black-600 opacity-0 absolute bottom-0 right-0 w-full h-full hover:opacity-80">
+                  <div className="absolute bottom-0">
+                    <div className=" flex w-fit justify-between items-end mb-3 mr-1">
+                      <span className="font-black rounded-tr-xl rounded-br-xl bg-gray-400 bg-opacity-[.8] w-fit p-[3px] pr-2  items-center">
+                        IMDb
+                      </span>
+                      <span className="rounded-tl-xl rounded-bl-xl pl-2 bg-gray-400 bg-opacity-[.7] p-[3px]">
+                        {rate}/10
+                      </span>
                     </div>
+                    <p className="mb-3 mr-1">{genre}</p>
+                    <p className="mb-3 mr-1">
+                      {year} - {country} {isDub && " - دوبله شده"}
+                    </p>
                   </div>
-                )} */}
+                </div>
+                <p className="truncate mt-4">{faName}</p>
               </div>
-            </div>
-          );
-        })}
+            );
+          }
+        )}
       </div>
     </div>
   );
