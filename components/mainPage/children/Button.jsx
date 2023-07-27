f89b5c4df6f5abcc7dae6774ae981c63b98847b5
narges <nargesmanafii@gmail.com>
2023-07-27 19:13:55 +0330

    ref :refactor add buttons part of children

---
+++ components/mainPage/children/Button.jsx
@@ -0,0 +1,9 @@
+const Buttons = () => {
+    return (
+      <div>
+        <button className="py-[.6rem] px-4 text-[.7rem] m-3 mb-8 rounded-lg bg-gray-200 fond-bold transition delay-150  ease-in-out hover:bg-gray-300 font-iran-sans-medium">فیلیمو کودک بساز</button>
+        <button className="py-[.6rem] px-4 text-[.7rem] rounded-lg bg-gray-200 transition delay-150  ease-in-out hover:bg-gray-300 font-iran-sans-medium fond-bold">تماشای فیلیمو کودک</button>
+      </div>
+    );
+  };
+  export default Buttons;
