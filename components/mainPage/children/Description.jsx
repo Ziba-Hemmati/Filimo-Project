6a89bd436dbcd929f6eaf8f9835d10628e132622
narges <nargesmanafii@gmail.com>
2023-07-27 19:38:36 +0330

    ref :refactor style part of children

--- components/mainPage/children/Description.jsx
+++ components/mainPage/children/Description.jsx
@@ -5,7 +5,7 @@ const Description = () => {
         <div className="text-white text-right pb-6 text-xl">
           <Title>دنیایی متنوع از فیلم و کارتون‌های کودکانه</Title>
         </div>
-        <div className="text-gray-300 text-sm pb-8">
+        <div className="text-gray-300 text-[.82rem] pb-8">
           <p>ساخت فیلیمو کودک این امکان را می‌دهد که با انتخاب سن فرزندتان، یک فضای امن برای تماشای کودک بسازید؛ یعنی محتوای مخصوص سن خودش را ببیند و به بقیه فیلم و سریال‌ها دسترسی نداشته‌باشد.</p>
         </div>
       </div>
