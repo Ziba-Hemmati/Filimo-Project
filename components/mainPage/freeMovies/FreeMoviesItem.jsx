74a307771317cae82010ca51b628c3bd55889d3d
Farzad Dehghani <farzad.dehghani1379@gmail.com>
2023-07-27 13:32:28 +0330

    Fix: edit fetch data

--- components/mainPage/freeMovies/FreeMoviesItem.jsx
+++ components/mainPage/freeMovies/FreeMoviesItem.jsx
@@ -1,4 +1,3 @@
-"use client";
 import Link from 'next/link'
 
 const FreeMoviesPosterItem = ({ id, poster, faName }) => {
--- components/mainPage/freeMovies/getFreeMovies.js
+++
@@ -1,9 +0,0 @@
-import { getLocalData } from "@/lib/localdata";
-
-const getFreeMovies = async () => {
-  const data = await getLocalData();
-  const movies = data.movies;
-  return movies.filter(movie => movie.category === "animation");
-};
-
-export default getFreeMovies();
