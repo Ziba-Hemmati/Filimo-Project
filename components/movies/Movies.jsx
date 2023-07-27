d93953b96d95e0584415e4be8c115538d73febc8
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-27 15:23:14 +0330

    feat: add hover to movies

--- components/movies/Movies.jsx
+++ components/movies/Movies.jsx
@@ -2,22 +2,29 @@
 import { useState } from "react";
 
 const Movies = ({ movie }) => {
-  const [isShow, setIsShow] = useState(false);
+  // const [isShow, setIsShow] = useState(false);
 
   return (
     <div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 text-white p-[10px] ">
         {movie.map(({ poster, id, faName, genre, year, country, isDub }) => {
           return (
-            <div key={id} className="">
+            <div key={id} className=" min-h-[200px]">
               <div className="relative">
                 <img
-                  className="w-[100%] h-[100%] rounded-[4px] p-[5px]"
+                  className="w-full h rounded-[4px] p-[5px]"
                   src={poster}
-                  onMouseOver={() => setIsShow((prev) => !prev)}
+                  // onMouseOver={() => setIsShow((prev) => !prev)}
                 />
                 <p className="text-[.7rem]">{faName}</p>
-                {isShow && (
+                <div className="bg-black-950 opacity-0 absolute top-0 right-0 w-full h-full ">
+                  <p>{genre}</p>
+                  <p>
+                    {year} - {country} {isDub && " - دوبله شده"}
+                  </p>
+                </div>
+
+                {/* {isShow && (
                   <div
                     key={id}
                     className="bg-black-600 bg-opacity-[.6] absolute w-[100%] h-[100%] z-10 top-[5px] right-[5px] text-[.7rem] "
@@ -29,7 +36,7 @@ const Movies = ({ movie }) => {
                       </p>
                     </div>
                   </div>
-                )}
+                )} */}
               </div>
             </div>
           );
