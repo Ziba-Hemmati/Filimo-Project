4500f5dbf341adf7ac1a2da5860056847b32fadf
Farzad Dehghani <farzad.dehghani1379@gmail.com>
2023-07-29 02:08:03 +0330

    ref: refactor freemovies slider

--- components/mainPage/freeMovies/FreeMoviesItem.jsx
+++ components/mainPage/freeMovies/FreeMoviesItem.jsx
@@ -1,22 +1,57 @@
+"use client"
 import Link from 'next/link'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay,Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
-const FreeMoviesPosterItem = ({ id, poster, faName }) => {
+const FreeMoviesPosterItem = ({ data }) => {
     
     return (
-    <li key={id}
-    className={"opacity-80 hover:opacity-100 mx-2.5 lg2:m-2"}
-    >
-      <Link href={`/${faName}`}>
-         <img
-           className={"w-[130px] h-[175px] rounded-md md:w-[165px] md:h-[220px]"}
-           src={poster}
-           alt={faName}
-           width={200}
-           height={200}
-         />
-         <p className={"text-white w-[160px] text-[0.7rem] mt-1.5"}>انیمیشن - {faName}</p>
-      </Link>
-    </li>
+      <Swiper
+      spaceBetween={50}
+      breakpoints={{
+         300: {
+            slidesPerView: 3,
+         },
+         700: {
+             slidesPerView: 4,
+         },
+         1100: {
+             slidesPerView: 5,
+         },
+         1200: {
+             slidesPerView: 6,
+         },
+      }}
+      navigation
+      loop
+      autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+      }}
+      modules={[Autoplay,Navigation]}
+  >
+
+      {data.map(({ id, poster, faName }) => (
+          <SwiperSlide key={id}>
+             <li className={"w-fit opacity-80 hover:opacity-100"}>
+              <Link href={`/${faName}`}>
+                 <img
+                  className={"w-[130px] h-[175px] rounded-md md:w-[165px] md:h-[220px]"}
+                   src={poster}
+                   alt={faName}
+                 />
+                 <p className={"truncate text-white w-[160px] text-[0.8rem] mt-2"}>
+                 انیمیشن - {faName}
+                 </p>
+              </Link>
+             </li>
+
+          </SwiperSlide>
+      ))}
+  </Swiper>
+   
   );
 };
 
