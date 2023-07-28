8e2e4ec1f7448b592c28cb1dcac3823279e97b2f
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-29 00:33:34 +0330

    feat: add styles to banner

--- components/eachMovie/EachMovieBanner.jsx
+++ components/eachMovie/EachMovieBanner.jsx
@@ -1,4 +1,16 @@
 import { getLocalData } from "@/lib/localdata";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleUp,
+  faAngleDown,
+  faBookmark,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
+
+// *************************************************************************
 
 const EachMovieBanner = async ({ mark }) => {
   const data = await getLocalData();
@@ -13,7 +25,6 @@ const EachMovieBanner = async ({ mark }) => {
     enName,
     rate,
     age,
-    factors,
     time,
     country,
     year,
@@ -23,30 +34,48 @@ const EachMovieBanner = async ({ mark }) => {
     genre,
     banner,
     link,
-    factors : {director}
+    factors: { director },
   } = target;
-  // const { director } = factors;
 
   return (
-    <div className="text-white">
-      <img src={banner} alt="banner" />
-
-      <div>
-        <p>فیلم {faName} </p>
-        <p>{enName} </p>
-        <p>{rate} </p>
-        <p> مناسب برای بالای {age} سال </p>
-        <p>کارگردان:{director}</p>
-        <p>
-          {time} - محصول {country} - {year} -{" "}
-          {isDub ? "دوبله شده" : "دوبله نشده"} -{" "}
-          {isSub ? "با زیرنویس" : "بدون زیرنویس"} - کیفیت {quality}{" "}
-        </p>
+    <div className="text-white w-full ">
+      <div
+        className="h-[90vh] flex flex-col items-center py-5"
+        style={{ backgroundImage: `url("${banner}")` }}
+      >
+        <iframe
+          src={link}
+          frameborder="0"
+          className="w-[148px] h-[198px] rounded-[4px] "
+        ></iframe>
+        <div className="text-[.9rem] text-center">
+          <div className="flex items-center justify-center pb-5 pt-3">
+            <p className="text-base">فیلم {faName} </p>
+            <FontAwesomeIcon
+              className="bg-white text-black rounded-full p-1 mr-2 "
+              icon={faAngleDown}
+            />
+            <FontAwesomeIcon
+              className="bg-white text-black rounded-full p-1 hidden "
+              icon={faAngleUp}
+            />
+          </div>
+          <p className="pb-3">{enName} </p>
+          <p className="pb-6">10/{rate} </p>
+          <p className="text-orange-400 mb-8"> مناسب برای بالای {age} سال </p>
+          <p className="hidden">کارگردان:{director}</p>
+          <p className="hidden">
+            {time} - محصول {country} - {year} -{" "}
+            {isDub ? "دوبله شده" : "دوبله نشده"} -{" "}
+            {isSub ? "با زیرنویس" : "بدون زیرنویس"} - کیفیت {quality}
+          </p>
+          <div className="hidden">{genre} </div>
+          <FontAwesomeIcon icon={faBookmark} />
+          <div className="mb-4">بعدا میبینم</div>
+          <FontAwesomeIcon icon={faHeart} />
+          <div>دوست داشتم</div>
+        </div>
       </div>
-      <div>{genre} </div>
-      <div>بعدا میبینم</div>
-
-      <iframe src={link} frameborder="0"></iframe>
     </div>
   );
 };
