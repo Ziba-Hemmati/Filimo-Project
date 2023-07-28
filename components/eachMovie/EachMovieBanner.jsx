1c956f1f51fc9e6369ac1c9d384320d455233367
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-28 18:48:53 +0330

    chore: add details

--- components/eachMovie/EachMovieBanner.jsx
+++ components/eachMovie/EachMovieBanner.jsx
@@ -3,17 +3,51 @@ import { getLocalData } from "@/lib/localdata";
 const EachMovieBanner = async ({ mark }) => {
   const data = await getLocalData();
   const movie = data.movies;
+
   const target = movie.find((item) => {
-    item.id === mark 
+    return item.id === Number(mark);
   });
+  const {
+    id,
+    faName,
+    enName,
+    rate,
+    age,
+    factors,
+    time,
+    country,
+    year,
+    isDub,
+    isSub,
+    quality,
+    genre,
+    banner,
+    link,
+  } = target;
+  const { director } = factors;
 
-  return <div className="to-white">
+  return (
+    <div className="text-white">
+      <img src={banner} alt="banner" />
 
-{
-  target && <div> {target.id} </div>
-}
+      <div>
+        <p>فیلم {faName} </p>
+        <p>{enName} </p>
+        <p>{rate} </p>
+        <p> مناسب برای بالای {age} سال </p>
+        <p>کارگردان:{director}</p>
+        <p>
+          {time} - محصول {country} - {year} -{" "}
+          {isDub ? "دوبله شده" : "دوبله نشده"} -{" "}
+          {isSub ? "با زیرنویس" : "بدون زیرنویس"} - کیفیت {quality}{" "}
+        </p>
+      </div>
+      <div>{genre} </div>
+      <div>بعدا میبینم</div>
 
-  </div>;
+      <iframe src={link} frameborder="0"></iframe>
+    </div>
+  );
 };
 
 export default EachMovieBanner;
