933d4fd4fe2a113b2e81a64fcca7b558c044a6a2
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-31 00:11:31 +0330

    chore: delete empty line

--- app/tag/page.js
+++ app/tag/page.js
@@ -1,10 +1,5 @@
-
 const Tag = () => {
-    return (
-        <div className="text-white">
-            not found
-        </div>
-    );
+  return <div className="text-white">not found</div>;
 };
 
 export default Tag;
