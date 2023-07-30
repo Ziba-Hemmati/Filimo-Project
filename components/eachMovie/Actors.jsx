e923cce9ef78dc14f4d5af479264f99658deadf4
narges <nargesmanafii@gmail.com>
2023-07-30 17:34:38 +0330

    ref:refactor change factors in part of each movie

--- components/eachMovie/Actors.jsx
+++ components/eachMovie/Actors.jsx
@@ -12,9 +12,9 @@ const Actors = async ({ mark }) => {
 
   const { actors } = target;
   return (
-    <div>
+    <div className="md:px-7">
       <Title className="mt-16 p-3 text-sm">بازیگران فیلم {target.faName}</Title>
-      <div className="flex flex-wrap m-4">
+      <div className="flex flex-wrap m-3">
         {actors.map((actor) => (
           <div key={actor.id} className=" mb-2 mr-2 items-center bg-black-fade-600 flex flex-row py-1 pr-2 pl-1 text-center rounded-3xl text-xs hover: hover:bg-gray-700 transition delay-100">
             <p >{actor.name}</p>
--- components/eachMovie/Factors.jsx
+++ components/eachMovie/Factors.jsx
@@ -3,33 +3,42 @@ import { getLocalData } from "@/lib/localdata";
 const Factors = async ({ mark }) => {
   const data = await getLocalData();
   const movie = data.movies;
-  const target = movie.find((item) => item.id === Number(mark));
 
-  if (!target) {
-    return null;
-  }
+  const target = movie.find((item) => {
+    return item.id === Number(mark);
+  });
 
-  const { director, producer, writer, dublor, musician } = target;
-  const factors = [
-    { name: 'کارگردان', value: director },
-    { name: 'تهیه‌کننده', value: producer },
-    { name: 'فیلم‌نامه‌نویس', value: writer },
-    { name: 'صداپیشه', value: dublor },
-    { name: 'آهنگساز', value: musician },
-  ];
-  
-  return (
-    <div>
-      <Title className="mt-10 p-3 text-sm">سایر عوامل فیلم {target.faName}</Title>
-      <div className="flex flex-wrap m-4">
-        {factors.map((factor) => (
-          <div key={factor.id} className="">
-            <p className="text-gray-800 ml-3 text-sm">{factor.name}</p>
-            <p>{factor.value}</p>
+  const { factors: { director = null },
+        factors: { writer = null },
+        factors: { producer = null },
+        factors: { cameraMan = null },
+        factors: { musician = null },
+        factors: { dublor = null },
+      } = target;
+
+const factors = [
+  { name: 'کارگردان', value: director },
+  { name: 'تهیه‌کننده', value: producer },
+  { name: 'فیلم‌نامه‌نویس', value: writer },
+  { name: 'صداپیشه', value: dublor },
+  { name: 'آهنگساز', value: musician },
+  { name: 'فیلم بردار' , value: cameraMan}
+];
+
+return (
+  <div className="md:px-7">
+    <Title className="mt-16 p-3 text-sm">بازیگران فیلم {target.faName}</Title>
+    <div className="flex flex-wrap m-3">
+      {factors.map((factor) => (
+        (factor.value !== null) && (
+          <div key={factor.id} className=" px-2.5 mb-5 font-iran-sans-medium">
+            <p className="text-gray-800 ml-3 text-xs mb-2 font-bold">{factor.name}</p>
+            <p className="text-[.65rem] ">{factor.value}</p>
           </div>
-          ))}
-      </div>
+        )
+      ))}
     </div>
+  </div>
   );
 };
 
