a4ebbc6197d8fe08e9788f5494c5e9cb8e99767a
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-30 23:31:23 +0330

    feat: add redux persist

--- redux/provider.jsx
+++ redux/provider.jsx
@@ -1,8 +1,13 @@
 "use client";
 
 import { Provider } from "react-redux";
-import store from "@/redux/store";
+import store, { persistor } from "@/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 export function Providers({ children }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor} >{children}</PersistGate>
+    </Provider>
+  );
 }
