074883b96dbf2d7f8a6b81337d75b9554ff7089b
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-31 00:09:08 +0330

    fix: fix rated route

--- app/(myLists)/rated/page.js
+++ app/(myLists)/rated/page.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Button from "@/components/myLists/Button";
-import EmptyList from "@/components/myLists/emptyList";
 import Like from "@/components/myLists/Like";
 
 const MyList = () => {
