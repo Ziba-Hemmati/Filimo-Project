513016e0f88cd8026b0840475a952f78a4810124
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-31 00:33:24 +0330

    feat: add redux persist

--- redux/store.js
+++ redux/store.js
@@ -1,14 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
 import bestOfImdbSlice from "@/redux/features/bestOfimdbSlice";
 import bookMarkSlice from "./features/bookMarkSlice";
 import likeSlice from "./features/likeSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const store = configureStore({
-  reducer: {
-    bestOfImdb: bestOfImdbSlice,
-    bookmark: bookMarkSlice,
-    like: likeSlice,
-  },
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const reducer = combineReducers({
+  bestOfImdb: bestOfImdbSlice,
+  bookmark: bookMarkSlice,
+  like: likeSlice,
 });
 
+const persistReducer1 = persistReducer(persistConfig, reducer);
+
+const store = configureStore({
+  reducer: persistReducer1,
+  middleware: [],
+});
+
+export const persistor = persistStore(store);
 export default store;
