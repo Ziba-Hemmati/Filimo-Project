4f0f03b9e66059e0af334e70b54ffd4295522261
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-31 00:21:56 +0330

    fix: fix categories page component

--- components/category/categories/CategoriesPage.jsx
+++ components/category/categories/CategoriesPage.jsx
@@ -5,76 +5,107 @@ import CategoryPoster from "@/components/category/categories/CategoriesPoster";
 const CategoriesPage = async () => {
   const data = await getLocalData();
   const movies = data.movies;
-  const dataa = movies.filter(movie => movie.category === "animation");
-  const datab = movies.filter(movie => movie.category === "adventure");
-  const datac = movies.filter(movie => movie.category === "comedy");
-  const datad = movies.filter(movie => movie.category === "action");
-  const datae = movies.filter(movie => movie.category === "drum");
-  const categories = data.categories;
-  
+  const dataa = movies.filter((movie) => movie.category === "animation");
+  const datab = movies.filter((movie) => movie.category === "adventure");
+  const datac = movies.filter((movie) => movie.category === "comedy");
+  const datad = movies.filter((movie) => movie.category === "action");
+  const datae = movies.filter((movie) => movie.category === "drum");
+
   return (
-    <div className={"flex flex-col overflow-hidden text-white my-12 px-5 sm:px-10"}>
-      <div className="flex justify-between w-full px-2">
-        <Title className="text-[14px] w-fit hover:text-orange-300 cursor-pointer">{categories[0]}</Title>
-        <p className="text-[11px] w-fit font-bold text-orange-300 mb- hover: cursor-pointer">مشاهده همه {'>'}</p>
+    <div
+      className={"flex flex-col overflow-hidden text-white my-12 px-5 sm:px-10"}
+    >
+      <div className="flex justify-between w-full">
+        <Title className="text-[0.9rem] w-fit hover:text-orange-300 cursor-pointer">
+          انیمیشن
+        </Title>
+        <Title className="text-[0.65rem] w-fit text-orange-300 mb- hover: cursor-pointer">
+          مشاهده همه {">"}
+        </Title>
       </div>
       <div className={"flex justify-center lg2:px-[10px]"}>
         <div
-          className={"w-full overflow-x-scroll lg:flex lg:justify-center lg2:overflow-hidden "}
+          className={
+            "w-full overflow-x-scroll lg:flex lg:justify-center lg2:overflow-hidden "
+          }
         >
           <CategoryPoster data={dataa} />
         </div>
       </div>
 
-      <div className="flex justify-between w-full px-2">
-       <Title className="text-[14px] w-fit hover:text-orange-300 cursor-pointer">{categories[1]}</Title>
-       <p className="text-[11px] w-fit font-bold text-orange-300 mb- hover: cursor-pointer">مشاهده همه {'>'}</p>
+      <div className="flex justify-between w-full">
+        <Title className="text-[0.9rem] w-fit hover:text-orange-300 cursor-pointer">
+          ماجراجویی
+        </Title>
+        <Title className="text-[0.65rem] w-fit text-orange-300 mb- hover: cursor-pointer">
+          مشاهده همه {">"}
+        </Title>
       </div>
       <div className={"flex justify-center lg2:px-[10px]"}>
         <div
-          className={"w-full overflow-x-scroll lg:flex lg:justify-center lg2:overflow-hidden "}
+          className={
+            "w-full overflow-x-scroll lg:flex lg:justify-center lg2:overflow-hidden "
+          }
         >
           <CategoryPoster data={datab} />
         </div>
       </div>
 
-      <div className="flex justify-between w-full px-2">
-        <Title className="text-[14px] w-fit hover:text-orange-300 cursor-pointer">{categories[2]}</Title>
-        <p className="text-[11px] w-fit font-bold text-orange-300 mb- hover: cursor-pointer">مشاهده همه {'>'}</p>
+      <div className="flex justify-between w-full">
+        <Title className="text-[0.9rem] w-fit hover:text-orange-300 cursor-pointer">
+          کمدی
+        </Title>
+        <Title className="text-[0.65rem] w-fit text-orange-300 mb- hover: cursor-pointer">
+          مشاهده همه {">"}
+        </Title>
       </div>
       <div className={"flex justify-center lg2:px-[10px]"}>
         <div
-          className={"w-full overflow-x-scroll lg:flex lg:justify-center lg2:overflow-hidden "}
+          className={
+            "w-full overflow-x-scroll lg:flex lg:justify-center lg2:overflow-hidden "
+          }
         >
           <CategoryPoster data={datac} />
         </div>
       </div>
 
-      <div className="flex justify-between w-full px-2">
-        <Title className="text-[14px] w-fit hover:text-orange-300 cursor-pointer">{categories[3]}</Title>
-        <p className="text-[11px] w-fit font-bold text-orange-300 mb- hover: cursor-pointer">مشاهده همه {'>'}</p>
+      <div className="flex justify-between w-full">
+        <Title className="text-[0.9rem] w-fit hover:text-orange-300 cursor-pointer">
+          اکشن
+        </Title>
+        <Title className="text-[0.65rem] w-fit text-orange-300 mb- hover: cursor-pointer">
+          مشاهده همه {">"}
+        </Title>
       </div>
       <div className={"flex justify-center lg2:px-[10px]"}>
         <div
-          className={"w-full overflow-x-scroll lg:flex lg:justify-center lg2:overflow-hidden "}
+          className={
+            "w-full overflow-x-scroll lg:flex lg:justify-center lg2:overflow-hidden "
+          }
         >
           <CategoryPoster data={datad} />
         </div>
       </div>
 
-      <div className="flex justify-between w-full px-2">
-        <Title className="text-[14px] w-fit hover:text-orange-300 cursor-pointer">{categories[4]}</Title>
-        <p className="text-[11px] w-fit font-bold text-orange-300 mb- hover: cursor-pointer">مشاهده همه {'>'}</p>
+      <div className="flex justify-between w-full">
+        <Title className="text-[0.9rem] w-fit hover:text-orange-300 cursor-pointer">
+          درام
+        </Title>
+        <Title className="text-[0.65rem] w-fit text-orange-300 mb- hover: cursor-pointer">
+          مشاهده همه {">"}
+        </Title>
       </div>
       <div className={"flex justify-center lg2:px-[10px]"}>
         <div
-          className={"w-full overflow-x-scroll lg:flex lg:justify-center lg2:overflow-hidden "}
+          className={
+            "w-full overflow-x-scroll lg:flex lg:justify-center lg2:overflow-hidden "
+          }
         >
           <CategoryPoster data={datae} />
         </div>
       </div>
-   </div>
- );
-}
+    </div>
+  );
+};
 
 export default CategoriesPage;
