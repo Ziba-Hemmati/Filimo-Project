01d46c45d730533841a9da3240ea606438021505
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-31 18:21:49 +0330

    fix: fix import empty list

--- components/myLists/BookMark.jsx
+++ components/myLists/BookMark.jsx
@@ -2,15 +2,11 @@
 import { useSelector } from "react-redux";
 import store from "@/redux/store";
 import Link from "next/link";
-import EmptyList from "./EmptyList";
 import PosterMovie from "../movies/PosterMovie";
+import EmptyList from "./emptyList";
 const BookMark = () => {
   const bookmark = useSelector((store) => store.bookmark);
-  //   console.log(bookmark);
   return (
-    // <div className="flex w-[400px] text-white p-[10px] text-[.7rem] lg:px-[30px]">
-    // <div className="flex">
-    // <div className="grid grid-cols-2 gap-3 md:gap-2 lg:gap-3 gap-y- sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 text-white p-[14px] text-[.7rem] lg:px-[30px]">
     <div
       className={`${
         bookmark.length
@@ -24,23 +20,22 @@ const BookMark = () => {
             console.log(id);
           }
           return (
-              <PosterMovie
-                key={id}
-                poster={poster}
-                genre={genre}
-                year={year}
-                country={country}
-                isDub={isDub}
-                rate={rate}
-                id={id}
-              />
+            <PosterMovie
+              key={id}
+              poster={poster}
+              genre={genre}
+              year={year}
+              country={country}
+              isDub={isDub}
+              rate={rate}
+              id={id}
+            />
           );
         })
       ) : (
         <EmptyList />
       )}
     </div>
-    // </div>
   );
 };
 
