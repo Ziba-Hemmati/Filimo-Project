350bbf0ea9c462b942eb7792b1ab5879917bd58f
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-31 13:37:02 +0330

    feat: add styles

--- components/eachMovie/EachMovieBanner.jsx
+++ components/eachMovie/EachMovieBanner.jsx
@@ -53,11 +53,11 @@ const EachMovieBanner = async ({ mark }) => {
           ></iframe>
 
           <div className="hidden md:inline-block">
-            <p className="text-base mb-8">فیلم {faName} </p>
+            <p className="text-base">فیلم {faName} </p>
             <p className="pb-3 text-[.8rem]">{enName} </p>
             <ImdbRate
               rate={rate}
-              className={"bg-orange-300 text-[.8rem] w-[82px] mb-2 "}
+              className={"bg-orange-300 text-[.8rem] w-[82px] mb-2"}
             />
             <p className="text-orange-400 mb-8 text-[.8rem]">
               {" "}
@@ -92,9 +92,9 @@ const EachMovieBanner = async ({ mark }) => {
             />
           </div>
         </div>
-        <div className="md:px-8 w-full">
+        <div className="md:px-8 w-full flex justify-center md:inline-block">
           <div className="hidden md:inline-block border border-t-gray-300 w-full xl:mt-8"></div>
-          <div className="flex-col flex text-center md:flex-row md:flex items-center mt-4 mb-2 xl:mb-4">
+          <div className="flex text-[.8rem] w-44 md:w-60 justify-between items-center text-center mt-4 mb-2 xl:mb-4">
             <WatchLater target={target} />
             <Like target={target} />
           </div>
--- components/eachMovie/WatchLater.jsx
+++ components/eachMovie/WatchLater.jsx
@@ -6,18 +6,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 import { setBookmark } from "@/redux/features/bookMarkSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 
 const WatchLater = ({ target }) => {
   const bookmark = useSelector((store) => store.bookmark);
   const dispatch = useDispatch();
+  const [isActive, setIsActive] = useState(false);
+  const handleClick = () => {
+    setIsActive((prev) => !prev);
+  };
   return (
     <div>
       <button
-        className="md:flex items-center"
-        onClick={() => dispatch(setBookmark(target))}
+        className={`md:flex items-center`}
+        onClick= { () => dispatch(setBookmark(target))}
       >
         <FontAwesomeIcon icon={faBookmark} />
-        <div className="mb-4 md:mb-0 md:mr-1">بعدا میبینم</div>
+        <div className="md:mr-1">بعدا میبینم</div>
       </button>
     </div>
   );
