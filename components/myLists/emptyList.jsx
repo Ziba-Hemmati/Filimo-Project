59b9c945b56cf40d6156b3415a2b0d112e93e45f
javad7721 <m.javad7721@gmail.com>
2023-07-31 13:59:56 +0330

    fix:Add text to empty list

--- components/myLists/emptyList.jsx
+++ components/myLists/emptyList.jsx
@@ -3,14 +3,19 @@ import Image from "next/image";
 
 const EmptyList = () => {
   return (
-      <Image
-        src="https://www.filimo.com/assets/web/ui/img-QYm1wdoO0Ei8zOsNXwQ3NA/empty-list.svg"
-        alt="emty-list"
-        width="100"
-        height="100"
-        className="w-[15%]"
-      />
-  );
+      <>
+          <div className="flex flex-col justify-center items-center mt-20">
+              <Image
+                  src="https://www.filimo.com/assets/web/ui/img-QYm1wdoO0Ei8zOsNXwQ3NA/empty-list.svg"
+                  alt="emty-list"
+                  width="100"
+                  height="100"
+                  className="w-[80%]"
+              />
+              <p className="text-sm text-gray-200">فیلمی پیدا نشد</p>
+          </div>
+      </>
+);
 };
 
 export default EmptyList;
