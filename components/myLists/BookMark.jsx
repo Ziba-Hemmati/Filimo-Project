0790e42c39cfeb56c6287aa6c1318c9799f7b05b
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-31 16:54:06 +0330

    feat: add styles to my lists page

--- components/myLists/BookMark.jsx
+++ components/myLists/BookMark.jsx
@@ -2,27 +2,30 @@
 import { useSelector } from "react-redux";
 import store from "@/redux/store";
 import Link from "next/link";
-import EmptyList from "./emptyList";
+import EmptyList from "./EmptyList";
 import PosterMovie from "../movies/PosterMovie";
 const BookMark = () => {
   const bookmark = useSelector((store) => store.bookmark);
   //   console.log(bookmark);
   return (
     // <div className="flex w-[400px] text-white p-[10px] text-[.7rem] lg:px-[30px]">
-    <div className="text-white flex items-center justify-center">
+    // <div className="flex">
+    <div className="grid grid-cols-2 gap-3 md:gap-2 lg:gap-3 gap-y- sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 text-white p-[14px] text-[.7rem] lg:px-[30px]">
       {bookmark.length ? (
         bookmark.map(({ poster, id, genre, year, country, isDub, rate }) => {
           return (
-            <PosterMovie
-              key={id}
-              poster={poster}
-              genre={genre}
-              year={year}
-              country={country}
-              isDub={isDub}
-              rate={rate}
-              id={id}
-            />
+            <div className="mt-10">
+              <PosterMovie
+                key={id}
+                poster={poster}
+                genre={genre}
+                year={year}
+                country={country}
+                isDub={isDub}
+                rate={rate}
+                id={id}
+              />
+            </div>
           );
         })
       ) : (
--- components/myLists/Like.jsx
+++ components/myLists/Like.jsx
@@ -4,34 +4,32 @@ import Image from "next/image";
 import { useSelector } from "react-redux";
 import store from "@/redux/store";
 import Link from "next/link";
-import EmptyList from "./emptyList";
+import EmptyList from "./EmptyList";
 import PosterMovie from "../movies/PosterMovie";
 const Like = () => {
   const like = useSelector((store) => store.like);
   //   console.log(bookmark);
   return (
-    <div className="flex w-[400px] text-white p-[10px] text-[.7rem] lg:px-[30px]">
-      <div className="text-white flex items-center justify-center">
-        {like.length ? (
-          like.map(({ poster, id, genre, year, country, isDub, rate }) => {
-            return (
-              <PosterMovie
-                key={id}
-                poster={poster}
-                genre={genre}
-                year={year}
-                country={country}
-                isDub={isDub}
-                rate={rate}
-                id={id}
-              />
-            );
-          })
-        ) : (
-          <EmptyList />
-        )}
-      </div>
-    </div>
+    <>
+      {like.length ? (
+        like.map(({ poster, id, genre, year, country, isDub, rate }) => {
+          return (
+            <PosterMovie
+              key={id}
+              poster={poster}
+              genre={genre}
+              year={year}
+              country={country}
+              isDub={isDub}
+              rate={rate}
+              id={id}
+            />
+          );
+        })
+      ) : (
+        <EmptyList />
+      )}
+    </>
   );
 };
 
