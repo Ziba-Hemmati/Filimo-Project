c3885b20a9bbfc96deec09fbf649c62099ccc518
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-07-31 18:13:08 +0330

    fix: soft reset changes

--- components/myLists/emptyList.jsx
+++ components/myLists/emptyList.jsx
@@ -3,13 +3,15 @@ import Image from "next/image";
 
 const EmptyList = () => {
   return (
+    <div div className="flex flex-col items-center mt-20">
       <Image
         src="https://www.filimo.com/assets/web/ui/img-QYm1wdoO0Ei8zOsNXwQ3NA/empty-list.svg"
         alt="emty-list"
-        width="100"
-        height="100"
-        className="w-[15%]"
+        width="250"
+        height="250"
       />
+      <p className="text-gray-200 text-sm mt-4">فیلمی پیدا نشد</p>
+    </div>
   );
 };
 
