b9824374cf602d53d572190c32b85679b2d4c2b1
masin1 <masin.mm@gmail.com>
2023-07-31 17:05:07 +0330

    feat: add movie comments

---
+++ components/eachMovie/EachMovieCommentItems.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const EachMovieCommentItems = ({id, name, date, text}) => {
+  return (
+    <div className="py-4 border-t border-[#333]">
+      <div className="flex items-start">
+        <div className="bg-[#afb42b] text-white w-8 h-8 rounded-full flex items-center justify-center">
+          <p className="text-center text-lg">U</p>
+        </div>
+
+        <div className="px-3">
+          <div className="flex pb-3">
+            <p className="text-[#D0D0D0] font-semibold text-xs">{name}</p>
+            <p className="text-[#BDBDBD] text-xs px-3">{date}</p>
+          </div>
+          <p className="text-[#f6f6f6] text-[10px]">{text}</p>
+        </div>
+      </div>
+    </div> 
+  );
+};
+
+export default EachMovieCommentItems;
