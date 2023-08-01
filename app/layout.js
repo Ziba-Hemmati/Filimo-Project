8a8c5bd90e2fea211df125a065cacf8dff4f133b
ma-abasian <abasianmohammadali@gmail.com>
2023-08-01 10:22:43 +0330

    fix: bug fixed

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 import "./globals.css";
 import Header from "@/components/mainPage/header/Header";
-import Footer from "@/components/mainPage/footer/Footer";
 import FixedFooter from "@/components/mainPage/fixedFooter/FixedFooter";
 import { Providers } from "@/redux/provider";
 
@@ -12,9 +11,6 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  // const data = await getLocalData();
-  // const category = data.categories;
-
   return (
     <html lang="en">
       <body className={inter.className}>
