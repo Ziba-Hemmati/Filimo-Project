04fd26a7d7f387eb4b519619a29f5af97f0e8e7c
Farzad Dehghani <farzad.dehghani1379@gmail.com>
2023-08-03 21:57:57 +0330

    ref: refactor link

--- components/mainPage/freeMovies/FreeMoviesItem.jsx
+++ components/mainPage/freeMovies/FreeMoviesItem.jsx
@@ -35,7 +35,7 @@ const FreeMoviesPosterItem = ({ data }) => {
       {data.map(({ id, poster, faName }) => (
           <SwiperSlide key={id}>
              <li className={"w-fit opacity-80 hover:opacity-100"}>
-              <Link href={`/${faName}`}>
+              <Link href={`/movies/${id}`}>
                  <img
                   className={"w-[130px] h-[175px] rounded-md md:w-[165px] md:h-[220px]"}
                    src={poster}
