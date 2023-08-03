3c0194e7f38aed80bdedce01038b98bc7c8090b5
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-08-03 19:04:51 +0330

    feat: add search logic

--- components/mainPage/header/Header.jsx
+++ components/mainPage/header/Header.jsx
@@ -8,6 +8,7 @@ import { getLocalData } from "@/lib/localdata";
 const Header = async () => {
   const data = await getLocalData();
   const categories = data.categories;
+  const movies = data.movies;
   return (
     <nav className="z-10 lg:py-2 xl:py-1 xl:flex xl:justify-center bg-black-950 bg-opacity-[.7] sticky top-0">
       <div className="items-center justify-center md:px-5 lg:flex lg:px-0">
@@ -15,7 +16,7 @@ const Header = async () => {
         <div className="flex flex-col lg:flex-row-reverse xl:justify-around xl:flex">
           <div className="flex justify-between items-center p-2 md:px-0 lg:pl-0 lg:pr-0">
             <MobileLogo />
-            <Search />
+            <Search movies={movies} />
             <MyLists />
           </div>
           <Menu categories={categories} />
--- components/mainPage/header/search/Search.jsx
+++ components/mainPage/header/search/Search.jsx
@@ -1,4 +1,24 @@
-const Search = () => {
+"use client";
+import Movies from "@/app/movies/page";
+import Link from "next/link";
+import { useCallback, useMemo, useState } from "react";
+
+const Search = ({movies}) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleChange = useCallback((event) => {
+    setSearchTerm(event.target.value);
+  }, []);
+
+  const searchByFaName = (search) => {
+    return movies.filter((item) =>
+      item.faName.includes(search)
+    );
+  };
+  const SEARCH_Movie = useMemo(() => {
+    searchByFaName(searchTerm);
+  }, [searchTerm]);
+  console.log(SEARCH_Movie);
+
   return (
     <>
       <input
@@ -6,7 +26,14 @@ const Search = () => {
         type="text"
         placeholder="جست و جو ..."
         className=" bg-black-700 w-1/2 placeholder:text-xs rounded focus:outline-none text-orange-600 xl:p-1 pr-2"
+        value={searchTerm}
+        onChange={handleChange}
       />
+
+      {/* {!!searchTerm &&
+        SEARCH_Movie.map((movie) => {
+          <Link href={`/movies/${id}`}></Link>;
+        })} */}
     </>
   );
 };
