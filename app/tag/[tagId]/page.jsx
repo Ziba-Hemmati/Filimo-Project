7d6682fd21ad13e0bbba233ca31560abfd5be07f
ma-abasian <abasianmohammadali@gmail.com>
2023-08-05 18:31:32 +0330

    feat: add filter to each movie page and fix bugs of BannerSlider in it

--- app/tag/[tagId]/page.jsx
+++ app/tag/[tagId]/page.jsx
@@ -1,12 +1,14 @@
-import { CategoriesPage, SubHeader } from "@/components";
+import { CategoriesPage } from "@/components";
 import CategoriesBanner from "@/components/category/categories/CategoriesBanner";
+import { getLocalData } from "@/lib/localdata";
+import { getTargetData } from "@/components/category/categories/getSortedMovies";
 
-const Category = ({ params }) => {
+const Category = async ({ params }) => {
+  const data = await getLocalData();
   return (
     <div className="text-white">
-      <CategoriesBanner eachCategory={params.tagId}/>
-      <SubHeader />
-      <CategoriesPage eachCategory={params.tagId} />
+      <CategoriesBanner eachCategory={params.tagId} />
+      <CategoriesPage data={data} eachCategory={params.tagId} />
     </div>
   );
 };
