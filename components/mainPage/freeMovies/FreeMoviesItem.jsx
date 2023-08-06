2c40fd290ce1c4be749c4128660fc6795110423e
Farzad Dehghani <farzad.dehghani1379@gmail.com>
2023-08-07 02:42:48 +0330

    add suggested movies

--- components/mainPage/freeMovies/FreeMoviesItem.jsx
+++ components/mainPage/freeMovies/FreeMoviesItem.jsx
@@ -4,20 +4,23 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
-import "../../../assets/css/swiperNavigation.css";
+import '../../../assets/css/swiperFreeMovies.css'
 
 const FreeMoviesPosterItem = ({ data }) => {
   return (
     <Swiper
-      spaceBetween={50}
+      spaceBetween={25}
       breakpoints={{
         300: {
+          slidesPerView: 2,
+        },
+        500: {
           slidesPerView: 3,
         },
-        700: {
+        650: {
           slidesPerView: 4,
         },
-        1100: {
+        800: {
           slidesPerView: 5,
         },
         1200: {
@@ -34,14 +37,14 @@ const FreeMoviesPosterItem = ({ data }) => {
     >
       {data.map(({ id, poster, faName }) => (
           <SwiperSlide key={id}>
-             <li className={"w-fit opacity-80 hover:opacity-100"}>
+             <li className={"w-full opacity-80 hover:opacity-100"}>
               <Link href={`/movies/${id}`}>
                  <img
-                  className={"w-[130px] h-[175px] rounded-md md:w-[165px] md:h-[220px]"}
+                  className={"w-full h-[220px] rounded-md sm:h-[240px] md:w-[170px] md:h-[220px]"}
                    src={poster}
                    alt={faName}
                  />
-                 <p className={"truncate text-white w-[130px] text-[0.8rem] mt-2 md:w-[165px]"}>
+                 <p className={"truncate text-white text-[0.8rem] mt-2"}>
                  انیمیشن - {faName}
                  </p>
               </Link>
