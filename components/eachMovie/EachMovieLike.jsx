7038b45e7b15255ef8c1aaaa8e1ffe93cee77e00
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-08-07 19:54:46 +0330

    feat: update styles

--- components/eachMovie/EachMovieLike.jsx
+++ components/eachMovie/EachMovieLike.jsx
@@ -15,7 +15,7 @@ const EachMovieLike = ({ target }) => {
       <button
         className={`md:flex items-center md:bg-gray-250 hover:bg-gray-200 hover:bg-opacity-[.3] transition-colors duration-300 md:bg-opacity-[.2] md:px-3 md:py-2 rounded-3xl ${
           isLiked &&
-          "text-orange-400 md:outline outline-2 outline-orange-400 hover:bg-orange-400 hover:bg-opacity-100 hover:text-black"
+          "text-orange-400 md:outline outline-2 outline-orange-400 hover:bg-orange-400 hover:bg-opacity-[1] hover:text-black"
         }`}
         onClick={() => dispatch(setLike(target))}
       >
