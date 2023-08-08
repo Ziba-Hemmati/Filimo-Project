d77652686d40ef4b8c10cd12e27ee1a730b4565c
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-08-09 00:52:00 +0330

    fix: fix missing bracket

--- components/eachMovie/Actors.jsx
+++ components/eachMovie/Actors.jsx
@@ -20,34 +20,37 @@ const Actors = async ({ mark }) => {
       return null;
     }
 
-  const { actors, faName } = target;
-  return (
-    <div className=" px-3 sm:px-0 md:px-8 xl:px-[9rem]">
-      <Title className="mt-16 p-3 text-sm">بازیگران فیلم {target.faName}</Title>
-      <div className="flex flex-wrap m-3">
-        {actors.map((actor) => (
-          <div
-            key={actor.id}
-            className=" mb-2 mr-2 items-center bg-black-fade-600 flex flex-row py-1 pr-2 pl-1 text-center rounded-3xl text-xs hover: hover:bg-gray-700 transition delay-100"
-          >
-            <p>{actor.name}</p>
-            {actor.image ? (
-              <img
-                src={actor.image}
-                className="w-[30px] h-[30px] rounded-full mr-3"
-                alt={faName}
-              />
-            ) : (
-              <FontAwesomeIcon
-                icon={faUser}
-                className="p-2 rounded-full mr-3  bg-gray-800"
-              />
-            )}
-          </div>
-        ))}
+    const { actors, faName } = target;
+    return (
+      <div className=" px-3 sm:px-0 md:px-8 xl:px-[9rem]">
+        <Title className="mt-16 p-3 text-sm">
+          بازیگران فیلم {target.faName}
+        </Title>
+        <div className="flex flex-wrap m-3">
+          {actors.map((actor) => (
+            <div
+              key={actor.id}
+              className=" mb-2 mr-2 items-center bg-black-fade-600 flex flex-row py-1 pr-2 pl-1 text-center rounded-3xl text-xs hover: hover:bg-gray-700 transition delay-100"
+            >
+              <p>{actor.name}</p>
+              {actor.image ? (
+                <img
+                  src={actor.image}
+                  className="w-[30px] h-[30px] rounded-full mr-3"
+                  alt={faName}
+                />
+              ) : (
+                <FontAwesomeIcon
+                  icon={faUser}
+                  className="p-2 rounded-full mr-3  bg-gray-800"
+                />
+              )}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Actors;
