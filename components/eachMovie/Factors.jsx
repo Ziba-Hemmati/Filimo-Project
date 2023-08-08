866cc4d893ce285f69d76957cfaefce20f450f37
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-08-09 01:05:29 +0330

    fix: fix factors component

--- components/eachMovie/Factors.jsx
+++ components/eachMovie/Factors.jsx
@@ -2,7 +2,6 @@
 // import Title from "../Title";
 import { getLocalData } from "@/lib/localdata";
 import EachMovieFactors from "./EachMovieFactors";
-import Title from "../Title";
 
 const Factors = async ({ mark }) => {
   const data = await getLocalData();
@@ -23,34 +22,7 @@ const Factors = async ({ mark }) => {
     //   { name: "فیلم بردار", key: "cameraMan" },
     // ];
 
-    return (
-      <div className="px-3 sm:px-0 md:px-8 xl:px-[9rem]">
-        <Title className="mt-14 p-3 text-sm">
-          سایر عوامل فیلم {target.faName}
-        </Title>
-        <div className="flex flex-wrap m-3">
-          {factorNames.map((factor) => {
-            const factorValue = factors[factor.key];
-            if (factorValue !== null && factorValue !== undefined) {
-              return (
-                <div
-                  key={factor.key}
-                  className="px-2.5 mb-5 font-iran-sans-medium"
-                >
-                  <p className="text-gray-800 ml-3 text-xs mb-2 font-bold">
-                    {factor.name}
-                  </p>
-                  <p className="text-[.65rem] hover:text-orange-300 cursor-pointer">
-                    {factorValue}
-                  </p>
-                </div>
-              );
-            }
-            return null;
-          })}
-        </div>
-      </div>
-    );
+    return <EachMovieFactors target={target} factors={factors} />;
   }
 };
 export default Factors;
