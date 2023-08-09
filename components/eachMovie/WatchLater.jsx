f0911d8980b3884f494eb8b69c54d42095c47650
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-08-09 11:16:37 +0330

    fix: comment watch later message

--- components/eachMovie/WatchLater.jsx
+++ components/eachMovie/WatchLater.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark, faClose } from "@fortawesome/free-solid-svg-icons";
 import { setBookmark } from "@/redux/features/bookMarkSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { useCallback, useState } from "react";
+// import { useCallback, useState } from "react";
 
 const WatchLater = ({ target }) => {
   const bookmark = useSelector((store) => store.bookmark);
@@ -11,17 +11,17 @@ const WatchLater = ({ target }) => {
   const isExist = bookmark.find((item) => {
     return item.id == target.id;
   });
-  const [toggle, setToggle] = useState(false);
-  const handleToggle = useCallback(() => {
-    setToggle(true);
-    setTimeout(() => {
-      setToggle(false);
-    }, 1500);
-  }, [toggle]);
+  // const [toggle, setToggle] = useState(false);
+  // const handleToggle = useCallback(() => {
+  //   setToggle(true);
+  //   setTimeout(() => {
+  //     setToggle(false);
+  //   }, 1500);
+  // }, [toggle]);
 
   return (
     <>
-      <div onClick={handleToggle}>
+      {/* <div onClick={handleToggle}> */}
         <button
           className={`md:flex items-center md:bg-gray-250 md:hover:bg-gray-200 md:hover:bg-opacity-[.3] transition-colors duration-300 md:bg-opacity-[.2] md:px-3 md:py-2 rounded-3xl ${
             isExist &&
@@ -32,8 +32,8 @@ const WatchLater = ({ target }) => {
           <FontAwesomeIcon icon={faBookmark} />
           <div className="md:mr-1 text-[11px]">بعدا میبینم</div>
         </button>
-      </div>
-      {toggle
+      {/* </div> */}
+      {/* {toggle
         ? isExist && (
             <div className="absolute left-3 top-[102px] md:top-[458px] lg:top-[430px] xl:top-[462px] xl2:top-[472px] md:left-8 xl:left-[178px] bg-green-500 p-4 rounded-md flex items-center">
               <FontAwesomeIcon
@@ -56,7 +56,7 @@ const WatchLater = ({ target }) => {
               <p>از لیست "بعدا میبینم" حذف شد.</p>
             </div>
           )
-        : ""}
+        : ""} */}
     </>
   );
 };
