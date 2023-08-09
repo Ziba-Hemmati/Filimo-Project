84acad949a79fa44d886c257695f302f6bfb2acc
Ziba-Hemmati <129059745+Ziba-Hemmati@users.noreply.github.com>
2023-08-09 11:00:48 +0330

    fix: comment like message

--- components/eachMovie/EachMovieLike.jsx
+++ components/eachMovie/EachMovieLike.jsx
@@ -11,17 +11,17 @@ const EachMovieLike = ({ target }) => {
   const isLiked = like.find((item) => {
     return item.id === target.id;
   });
-  const [toggle, setToggle] = useState(false);
-  const handleToggle = useCallback(() => {
-    setToggle(true);
-    setTimeout(() => {
-      setToggle(false);
-    }, 1500);
-  }, [toggle]);
+  // const [toggle, setToggle] = useState(false);
+  // const handleToggle = useCallback(() => {
+  //   setToggle(true);
+  //   setTimeout(() => {
+  //     setToggle(false);
+  //   }, 1500);
+  // }, [toggle]);
 
   return (
     <>
-      <div onClick={handleToggle}>
+      {/* <div onClick={handleToggle}> */}
         <button
           className={`md:flex items-center md:bg-gray-250 hover:bg-gray-200 hover:bg-opacity-[.3] transition-colors duration-300 md:bg-opacity-[.2] md:px-3 md:py-2 rounded-3xl ${
             isLiked &&
@@ -32,9 +32,9 @@ const EachMovieLike = ({ target }) => {
           <FontAwesomeIcon icon={faHeart} className="ml-1" />
           <div className="text-[11px]">دوست داشتم</div>
         </button>
-      </div>
+      {/* </div> */}
 
-      {toggle
+      {/* {toggle
         ? isLiked && (
             <div className="absolute left-3 top-[102px] md:top-[458px] lg:top-[430px] xl:top-[462px] xl2:top-[472px] md:left-8 xl:left-[178px] bg-green-500 p-4 rounded-md flex items-center">
               <FontAwesomeIcon
@@ -57,7 +57,7 @@ const EachMovieLike = ({ target }) => {
               <p>از لیست "دوست داشتم" حذف شد.</p>
             </div>
           )
-        : ""}
+        : ""} */}
     </>
   );
 };
