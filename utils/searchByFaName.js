c03b9d79988ab9f3786483a329e96a42f8780e08
amiiirm8 <nargesmanafii@gmail.com>
2023-08-11 01:42:49 +0330

    feat:update search enName

--- utils/searchByFaName.js
+++
@@ -1,7 +0,0 @@
-export const searchByFaName = (movies, searchTerm) => {
-  const searchTermLowerCase = searchTerm.toLowerCase();
-  return movies.filter((item) =>
-    item.faName.toLowerCase().includes(searchTermLowerCase) ||
-    item.enName.toLowerCase().includes(searchTermLowerCase)
-  );
-};
